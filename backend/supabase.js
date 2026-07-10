7e75edfb4939e5ba10a9a5966cd9f07c50c34ea7
xyoutubkax-glitch <xyoutubkax@gmail.com>
2026-07-11 01:31:37 +0300

    test version

--- backend/supabase.js
+++ backend/supabase.js
@@ -1,7 +1,7 @@
 const { createClient } = require("@supabase/supabase-js");
 
 const supabase = createClient(
-  "https://tipqcjweblagmpaipdlv.supabase.co/rest/v1/",
+  "https://tipqcjweblagmpaipdlv.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpcHFjandlYmxhZ21wYWlwZGx2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MjQ5ODk4NCwiZXhwIjoyMDk4MDc0OTg0fQ.ae3uV4puvnr8dKcRe9LdW3FNZ1yQwP62ZRAZtmgP_b4"
 );
 
