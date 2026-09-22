1520d266c1168f8dbe1a1de2ce746c36285a698b
xyoutubkax-glitch <xyoutubkax@gmail.com>
2026-09-22 14:57:26 +0300

    test version

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const BOT_TOKEN = "8818876066:AAGQdlYtpcnExiZkH0woIEaQ3CO587JYH-k";
+const BOT_TOKEN = "8900081605:AAGXNFt6mG8Nef--x2UmT4nbjHb3Pfdloxo";
 
 const ADMIN_CHAT_ID = 7130132807;
 const GROUP_CHAT_ID = -1004456053400;
